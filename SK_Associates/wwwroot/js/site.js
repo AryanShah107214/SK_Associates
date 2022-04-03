016c00c5c2188a021d9c98f6adb16634e718818d
AryanShah107214 <ac107214@avcol.school.nz>
2022-04-03 15:15:03 +1200

    review section now responsive

--- SK_Associates/wwwroot/js/site.js
+++ SK_Associates/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+//window.addEventListener('load', function() {
+//    spinnerWrapper.style.display = 'none';
+//}
+    
+//);
