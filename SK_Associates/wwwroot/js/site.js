a00e1fea322054071c127556999d92b8da5e93ca
AryanShah107214 <ac107214@avcol.school.nz>
2022-05-14 20:18:12 +1200

    Added cards to services and reviews section

--- SK_Associates/wwwroot/js/site.js
+++ SK_Associates/wwwroot/js/site.js
@@ -2,10 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-//let spinnerWrapper = document.querySelector('.spinner-wrapper');
 
-//window.addEventListener('load', function() {
-//    spinnerWrapper.style.display = 'none';
+
+//Get the button:
+//mybutton = document.getElementById("scrollDown");
+
+//// When the user scrolls down 20px from the top of the document, show the button
+//window.onscroll = function () { scrollFunction() };
+
+//function scrollFunction() {
+//    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//        mybutton.style.display = "block";
+//    } else {
+//        mybutton.style.display = "none";
+//    }
 //}
-    
-//);
+
+//// When the user clicks on the button, scroll to the top of the document
+//function scroll() {
+//    document.body.scrollTo = 100; // For Safari
+//    document.documentElement.scrollTo = 100; // For Chrome, Firefox, IE and Opera
+//}
