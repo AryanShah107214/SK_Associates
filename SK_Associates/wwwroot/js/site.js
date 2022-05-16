e4f343743eaacddf6396496eae61c23c77bc64f0
AryanShah107214 <ac107214@avcol.school.nz>
2022-05-16 20:37:23 +1200

    hosted website and fixed minor bugs

--- SK_Associates/wwwroot/js/site.js
+++ SK_Associates/wwwroot/js/site.js
@@ -2,24 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
-//Get the button:
-//mybutton = document.getElementById("scrollDown");
-
-//// When the user scrolls down 20px from the top of the document, show the button
-//window.onscroll = function () { scrollFunction() };
-
-//function scrollFunction() {
-//    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//        mybutton.style.display = "block";
-//    } else {
-//        mybutton.style.display = "none";
-//    }
-//}
-
-//// When the user clicks on the button, scroll to the top of the document
-//function scroll() {
-//    document.body.scrollTo = 100; // For Safari
-//    document.documentElement.scrollTo = 100; // For Chrome, Firefox, IE and Opera
-//}
